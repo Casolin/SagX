f98c3687580e7091f02cdac995b5e3b370e6fda9
Casolin <animecaster@gmail.com>
2026-05-25 21:58:03 +0100

    xqc

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -92,9 +92,7 @@ api.interceptors.response.use(
       } catch (err) {
         processQueue(err, null);
 
-        localStorage.removeItem("accessToken");
-
-        window.location.href = "/login";
+        console.log("Refresh failed, but NOT logging out immediately");
 
         return Promise.reject(err);
       } finally {

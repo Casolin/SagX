07a8fd276b78add9fcb1c47e15730d44ff53220f
Casolin <animecaster@gmail.com>
2026-06-12 22:31:56 +0100

    xqc

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,46 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      devOptions: {
-        enabled: false,
-      },
-      manifest: {
-        id: "/",
-        name: "SagX",
-        short_name: "SagX",
-        description: "Your Favorite Management Tool",
-        start_url: "/dashboard",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "/icons/icon-192.webp",
-            sizes: "192x192",
-            type: "image/webp",
-            purpose: "any maskable",
-          },
-          {
-            src: "/icons/icon-512.webp",
-            sizes: "512x512",
-            type: "image/webp",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
